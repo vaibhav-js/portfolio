2142a927a78a4a79ceefe7631b155b23dcf5976f
vaibhav-js <vaibhavrajpianist@gmail.com>
2024-05-19 11:31:36 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "StarBURG Pharmaceuticals",
+      des: "In need of any emergency get prescribed medicines at your doorstep",
+      img: "/project1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg"],
+      link: "https://starburg.in/",
+    },
+    {
+      id: 2,
+      title: "Instagram Clone",
+      des: "Make friends and post photos just like INSTAGRAM",
+      img: "/project2.png",
+      iconLists: ["/re.svg", "/firebase.svg", "/nodejs.svg"],
+      link: "https://social-app-vaibhav-nine.vercel.app/auth",
+    },
+    {
+      id: 3,
+      title: "Wedding Task Management",
+      des: "Assign your wedding tasks to different vendors like JIRA or ASANA",
+      img: "/project3.png",
+      iconLists: ["/re.svg", "/tail.svg", "/google.svg", "/nodejs.svg", "/psql.svg", "/firebase.svg"],
+      link: "https://wedding-task.web.app/",
+    },
+    {
+      id: 4,
+      title: "Amazon Clone",
+      des: "Clone of a shopping application AMAZON with authentication",
+      img: "/project4.png",
+      iconLists: ["/re.svg", "/css.svg", "/firebase.svg", "/nodejs.svg", "/stripe.svg"],
+      link: "https://clone-fae3f.web.app/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Vaibhav was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project.",
+      name: "Aman",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "VaibhaV's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vaibhav is the ideal partner.",
+      name: "Ayush",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Vaibhav's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vaibhav is the ideal partner.",
+      name: "Akarsh",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Vaibhav was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Vaibhav's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vaibhav is the ideal partner.",
+      name: "Shrey",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Vaibhav was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Vaibhav's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vaibhav is the ideal partner.",
+      name: "Shahbaaz",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: 'https://github.com/vaibhav-js/',
+    },
+    {
+      id: 2,
+      img: "/drive.svg",
+      link: 'https://drive.google.com/file/d/1rz2y6fq6MDUYrRdwfkOJW1LZPQka6CnO/view?usp=drive_link',
+    },
+    {
+      id: 3,
+      img: "/linktile.svg",
+      link: 'https://www.linkedin.com/in/vaibhavraj174/',
+    },
+  ];
